5ee86524c0273ef443b640dd7b5aced4fb1d40b3
dst1213 <dst1213@126.com>
2023-07-06 08:39:14 +0800

    v0.7 增加繁中识别

--- test/html_clean.py
+++ test/html_clean.py
@@ -24,8 +24,40 @@ url = "https://scholars.lib.ntu.edu.tw/cris/rp/rp06754"
 # url = "https://www.uchicagomedicine.org/find-a-physician/physician/marina-chiara-garassino#"
 # url = "https://www.bcm.edu/people-search/thomas-kosten-24837"
 # url = "https://profiles.uchicago.edu/profiles/display/37485"
-response = requests.get(url,verify=False)
-content = cleaner.clean_html(response.text)
+
+
+# response = requests.get(url,verify=False)
+
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import requests
+
+def get_html_by_sn(url):
+    # 获取所有展示的文本内容和原始结构
+    # 引入浏览器配置
+    options = webdriver.ChromeOptions()
+    # 设置无头模式
+    options.add_argument('--headless')  # 不打开浏览器
+    # 启动浏览器实例，添加配置信息
+    browser = webdriver.Chrome(options=options)
+    # browser = webdriver.Chrome()
+    browser.get(url)
+    html_text = browser.page_source
+    soup = BeautifulSoup(html_text, 'html.parser')
+    text = soup.get_text()
+    output = ""
+    for t in text.split("\n"):
+        if t.replace(" ", "").replace("\t", "") == "":
+            continue
+        output += t
+        output += "\n"
+    browser.quit()
+    return soup, output, html_text
+
+_,_,html_text = get_html_by_sn(url)
+
+# content = cleaner.clean_html(response.text)
+content = cleaner.clean_html(html_text)
 
 import htmlmin
 content = htmlmin.minify(content, remove_comments=True, remove_all_empty_space=True)
